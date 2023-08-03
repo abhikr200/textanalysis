7b85bae981d49b7819d118676897bda67fdd6f28
abhishek <abhi.rnc98@gmail.com>
2023-08-03 14:59:55 +0530

    Updates

---
+++ precache-manifest.778567ae4be0f916d0aefc6ec7df4820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df09e45096745e532011",
+    "url": "/textanalysis/static/css/main.0e49d618.chunk.css"
+  },
+  {
+    "revision": "df09e45096745e532011",
+    "url": "/textanalysis/static/js/main.df09e450.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/textanalysis/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "e344360258642d673038",
+    "url": "/textanalysis/static/js/2.e3443602.chunk.js"
+  },
+  {
+    "revision": "199ea4e1eeb60001f39c",
+    "url": "/textanalysis/static/js/runtime~main.199ea4e1.js"
+  },
+  {
+    "revision": "2deb2f74ebbc7a49c1e9b085ae030cae",
+    "url": "/textanalysis/index.html"
+  }
+];
